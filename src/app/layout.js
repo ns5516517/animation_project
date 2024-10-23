1b0db7518a417318cb91f37efa2ba9696c6e0383
ns5516517 <63893259+ns5516517@users.noreply.github.com>
2024-10-23 20:26:23 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,4 @@
 import localFont from "next/font/local";
-import "./globals.css";
 import Header from "./components/header";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './public/sass/helper/base.scss';
